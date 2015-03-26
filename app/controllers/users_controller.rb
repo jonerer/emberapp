e66b5b1e4ec53ba3bc4cdcd00ceaabaae8ef8502
jon <jon@debian>
2015-03-26 06:51:38 +0100

    boom

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,8 @@ class UsersController < ApplicationController
   def index
     render json: User.all
   end
+
+  def me
+    render json: current_user
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users do
+    collection do
+      get :me
+    end
+  end
 
   resources :posts
 
